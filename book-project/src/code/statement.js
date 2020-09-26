36a94a1186ccc2ba93548ddcf4d65f3fe8458498
dalong <dalong0514@126.com>
2020-09-26 23:33:00 +0800

    1.6-重构

--- book-project/src/code/statement.js
+++ book-project/src/code/statement.js
@@ -1,71 +1,8 @@
 import { invoices, plays } from '@/code/data'
 
-function amountFor(aPerformance) {
-  let result = 0
-
-  switch (playFor(aPerformance).type) {
-    case 'tragedy':
-      result = 40000
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30)
-      }
-      break
-    case 'comedy':
-      result = 30000
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20)
-      }
-      result += 300 * aPerformance.audience
-      break
-    default:
-      throw new Error(`unkown type: ${playFor(aPerformance).type}`)
-  }
-  return result
-}
-
-function playFor(aPerformance) {
-  return plays[aPerformance.playID]
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0
-  // add volume credits
-  result += Math.max(aPerformance.audience - 30, 0)
-  // add extra credit for every ten comedy attendees
-  if (playFor(aPerformance).type === 'comedy') {
-    result += Math.floor(aPerformance.audience / 5)
-  }
-  return result
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumIntegerDigits: 2
-  }).format(aNumber / 100)
-}
-
-function totalVolumeCredits() {
-  let volumeCredits = 0
-  for (const perf of invoices.performances) {
-    // add volume credits
-    volumeCredits += volumeCreditsFor(perf)
-  }
-  return volumeCredits
-}
-
-function totalAmount() {
-  let result = 0
-  for (const perf of invoices.performances) {
-    result += amountFor(perf)
-  }
-  return result
-}
-
-function renderPlainText() {
-  let result = `Statement for ${invoices.customer}\n`
-  for (const perf of invoices.performances) {
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`
+  for (const perf of data.performances) {
     // print line for this order
     result += `${playFor(perf).name}: ${usd(amountFor(perf))}(${perf.audience} seats)\n`
   }
@@ -74,8 +11,74 @@ function renderPlainText() {
   result += `You earned ${totalVolumeCredits()} credits\n`
   console.log(result)
   return usd(totalAmount())
+
+  function amountFor(aPerformance) {
+    let result = 0
+
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`unkown type: ${playFor(aPerformance).type}`)
+    }
+    return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0
+    // add volume credits
+    result += Math.max(aPerformance.audience - 30, 0)
+    // add extra credit for every ten comedy attendees
+    if (playFor(aPerformance).type === 'comedy') {
+      result += Math.floor(aPerformance.audience / 5)
+    }
+    return result
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumIntegerDigits: 2
+    }).format(aNumber / 100)
+  }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0
+    for (const perf of data.performances) {
+      // add volume credits
+      volumeCredits += volumeCreditsFor(perf)
+    }
+    return volumeCredits
+  }
+
+  function totalAmount() {
+    let result = 0
+    for (const perf of data.performances) {
+      result += amountFor(perf)
+    }
+    return result
+  }
 }
 
 export function statement() {
-  return renderPlainText()
+  const statementData = {}
+  statementData.customer = invoices.customer
+  statementData.performances = invoices.performances
+  return renderPlainText(statementData)
 }
