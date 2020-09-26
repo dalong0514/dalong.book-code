fe9e1a6c4b83b84e74b5dcb9a073f33d61e255b3
dalong <dalong0514@126.com>
2020-09-26 14:53:13 +0800

    测试练习重构

---
+++ book-project/tests/unit/jestStudy.spec.js
@@ -0,0 +1,18 @@
+import { sum } from '@/code/jestStudy'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
+
+test('object assignment', () => {
+  const data = { one: 1 }
+  data.two = 2
+  expect(data).toEqual({
+    one: 1,
+    two: 2
+  })
+})
+
+test('string test using Re', () => {
+  expect('dalong').toMatch(/^da.*/)
+})
