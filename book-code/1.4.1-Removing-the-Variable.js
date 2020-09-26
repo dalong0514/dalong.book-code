acb748eaa1febe869fb8f124e7dde035a5626ac0
dalong <dalong0514@126.com>
2020-09-26 19:13:10 +0800

    1.4.1-Removing the play Variable

---
+++ book-code/1.4.1-Removing-the-Variable.js
@@ -0,0 +1,55 @@
+import { invoices, plays } from '@/code/data'
+
+function amountFor(aPerformance) {
+  let result = 0
+
+  switch (playFor(aPerformance).type) {
+    case 'tragedy':
+      result = 40000
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30)
+      }
+      break
+    case 'comedy':
+      result = 30000
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20)
+      }
+      result += 300 * aPerformance.audience
+      break
+    default:
+      throw new Error(`unkown type: ${playFor(aPerformance).type}`)
+  }
+  return result
+}
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID]
+}
+
+export function statement() {
+  let totalAmount = 0
+  let volumeCredits = 0
+  let result = `Statement for ${invoices.customer}\n`
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumIntegerDigits: 2
+  }).format
+  for (const perf of invoices.performances) {
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0)
+    // add extra credit for every ten comedy attendees
+    if (playFor(perf).type === 'comedy') {
+      volumeCredits += Math.floor(perf.audience / 5)
+    }
+
+    // print line for this order
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)}(${perf.audience} seats)\n`
+    totalAmount += amountFor(perf)
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `You earned ${volumeCredits} credits\n`
+  console.log(result)
+  return format(totalAmount / 100)
+}
