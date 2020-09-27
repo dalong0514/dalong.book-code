fcca17f64745404d8b36eb0d02a021b4d225ed1c
dalong <dalong0514@126.com>
2020-09-27 08:51:45 +0800

    1.6-重构

--- book-project/src/code/statement.js
+++ book-project/src/code/statement.js
@@ -4,36 +4,13 @@ function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`
   for (const perf of data.performances) {
     // print line for this order
-    result += `${perf.play.name}: ${usd(amountFor(perf))}(${perf.audience} seats)\n`
+    result += `${perf.play.name}: ${usd(perf.amount)}(${perf.audience} seats)\n`
   }
 
   result += `Amount owed is ${usd(totalAmount())}\n`
   result += `You earned ${totalVolumeCredits()} credits\n`
   console.log(result)
   return usd(totalAmount())
-
-  function amountFor(aPerformance) {
-    let result = 0
-
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`unkown type: ${aPerformance.play.type}`)
-    }
-    return result
-  }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0
@@ -66,7 +43,7 @@ function renderPlainText(data) {
   function totalAmount() {
     let result = 0
     for (const perf of data.performances) {
-      result += amountFor(perf)
+      result += perf.amount
     }
     return result
   }
@@ -80,11 +57,35 @@ export function statement() {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance)
-    result.play = playFor(aPerformance)
+    result.play = playFor(result)
+    result.amount = amountFor(result)
     return result
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0
+
+    switch (aPerformance.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`unkown type: ${aPerformance.play.type}`)
+    }
+    return result
   }
 }
